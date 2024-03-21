a60d6fe9a9fa42b273c6c7e3eb3b08990542209d
Hamza11067 <hamzakhalid1067@gmail.com>
2024-03-21 20:01:16 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+import InputBox from './components/InputBox.jsx'
+import useCurrency from './hooks/useCurrency.jsx'
+
+function App() {
+  const [amount, setAmount] = useState(0)
+  const [from, setFrom] = useState("usd")
+  const [to, setTo] = useState("pkr")
+  const [convertedAmount, setConvertedAmount] = useState(0)
+  const currencyInfo = useCurrency(from)
+  const options = Object.keys(currencyInfo)
+  const convert = () => {
+    setConvertedAmount((amount * currencyInfo[to]).toFixed(2))
+  }
+
+  return (
+    <div className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat bg-[#87c0cd]">
+        <div className="w-full ">
+            <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        convert()
+                       
+                    }}
+                >
+                    <div className="w-full mb-1">
+                        <InputBox
+                            label="From"
+                            amount={amount}
+                            currencyOptions={options}
+                            onCurrencyChange={(currency) => setFrom(currency)}
+                            selectCurrency={from}
+                            onAmountChange={(amount) => setAmount(amount)}
+                        />
+                    </div>
+                    <div className="w-full mt-1 mb-4">
+                        <InputBox
+                            label="To"
+                            amount={convertedAmount}
+                            currencyOptions={options}
+                            onCurrencyChange={(currency) => setTo(currency)}
+                            selectCurrency={to}
+                            amountDisable
+                        />
+                    </div>
+                    <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
+                        Convert {from.toUpperCase()} to {to.toUpperCase()}
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+);
+}
+
+export default App
